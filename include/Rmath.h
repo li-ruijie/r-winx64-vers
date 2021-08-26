573d570addceb82a866f97f4114fd74419e4b235
RJ <ruijie@git>
2021-08-26 12:39:32 +0800

    3.0.1

--- include/Rmath.h
+++ include/Rmath.h
@@ -40,7 +40,7 @@
 /*-- Mathlib as part of R --  define this for standalone : */
 /* #undef MATHLIB_STANDALONE */
 
-#define R_VERSION_STRING "3.0.0"
+#define R_VERSION_STRING "3.0.1"
 
 #ifndef HAVE_EXPM1
 # define HAVE_EXPM1 1
--- include/Rversion.h
+++ include/Rversion.h
@@ -6,17 +6,17 @@
 extern "C" {
 #endif
 
-#define R_VERSION 196608
-#define R_NICK "Masked Marvel"
+#define R_VERSION 196609
+#define R_NICK "Good Sport"
 #define R_Version(v,p,s) (((v) * 65536) + ((p) * 256) + (s))
 #define R_MAJOR  "3"
-#define R_MINOR  "0.0"
+#define R_MINOR  "0.1"
 #define R_STATUS ""
 #define R_YEAR   "2013"
-#define R_MONTH  "04"
-#define R_DAY    "03"
-#define R_SVN_REVISION 62481
-#define R_FILEVERSION    3,00,62481,0
+#define R_MONTH  "05"
+#define R_DAY    "16"
+#define R_SVN_REVISION 62743
+#define R_FILEVERSION    3,01,62743,0
 
 #ifdef __cplusplus
 }
