bebd76271282bdbd2589e0edfad69ad61ee2bd43
RJ <ruijie@git>
2021-08-26 13:00:32 +0800

    3.3.2

--- include/Rversion.h
+++ include/Rversion.h
@@ -6,17 +6,17 @@
 extern "C" {
 #endif
 
-#define R_VERSION 197377
-#define R_NICK "Bug in Your Hair"
+#define R_VERSION 197378
+#define R_NICK "Sincere Pumpkin Patch"
 #define R_Version(v,p,s) (((v) * 65536) + ((p) * 256) + (s))
 #define R_MAJOR  "3"
-#define R_MINOR  "3.1"
+#define R_MINOR  "3.2"
 #define R_STATUS ""
 #define R_YEAR   "2016"
-#define R_MONTH  "06"
-#define R_DAY    "21"
-#define R_SVN_REVISION 70800
-#define R_FILEVERSION    3,31,70800,0
+#define R_MONTH  "10"
+#define R_DAY    "31"
+#define R_SVN_REVISION 71607
+#define R_FILEVERSION    3,32,71607,0
 
 #ifdef __cplusplus
 }
