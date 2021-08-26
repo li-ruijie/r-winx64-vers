86abd047670303241508f972813c9bea9ce8853c
RJ <ruijie@git>
2021-08-26 13:25:12 +0800

    3.6.0

--- include/Rversion.h
+++ include/Rversion.h
@@ -6,17 +6,17 @@
 extern "C" {
 #endif
 
-#define R_VERSION 197891
-#define R_NICK "Great Truth"
+#define R_VERSION 198144
+#define R_NICK "Planting of a Tree"
 #define R_Version(v,p,s) (((v) * 65536) + ((p) * 256) + (s))
 #define R_MAJOR  "3"
-#define R_MINOR  "5.3"
+#define R_MINOR  "6.0"
 #define R_STATUS ""
 #define R_YEAR   "2019"
-#define R_MONTH  "03"
-#define R_DAY    "11"
-#define R_SVN_REVISION 76217
-#define R_FILEVERSION    3,53,76217,0
+#define R_MONTH  "04"
+#define R_DAY    "26"
+#define R_SVN_REVISION 76424
+#define R_FILEVERSION    3,60,76424,0
 
 #ifdef __cplusplus
 }
