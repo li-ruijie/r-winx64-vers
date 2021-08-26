36a54c99b2a7a50bcc4cf77dd59464fea5131dba
RJ <ruijie@git>
2021-08-26 13:45:59 +0800

    4.0.2

--- include/Rversion.h
+++ include/Rversion.h
@@ -6,17 +6,17 @@
 extern "C" {
 #endif
 
-#define R_VERSION 262145
-#define R_NICK "See Things Now"
+#define R_VERSION 262146
+#define R_NICK "Taking Off Again"
 #define R_Version(v,p,s) (((v) * 65536) + ((p) * 256) + (s))
 #define R_MAJOR  "4"
-#define R_MINOR  "0.1"
+#define R_MINOR  "0.2"
 #define R_STATUS ""
 #define R_YEAR   "2020"
 #define R_MONTH  "06"
-#define R_DAY    "06"
-#define R_SVN_REVISION 78648
-#define R_FILEVERSION    4,01,78648,0
+#define R_DAY    "22"
+#define R_SVN_REVISION 78730
+#define R_FILEVERSION    4,02,78730,0
 
 #ifdef __cplusplus
 }
