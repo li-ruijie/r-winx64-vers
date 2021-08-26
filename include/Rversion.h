106dbbbc86feaf074f3f0770ec06e27084b6581a
RJ <ruijie@git>
2021-08-26 13:09:56 +0800

    3.4.3

--- include/Rversion.h
+++ include/Rversion.h
@@ -6,17 +6,17 @@
 extern "C" {
 #endif
 
-#define R_VERSION 197634
-#define R_NICK "Short Summer"
+#define R_VERSION 197635
+#define R_NICK "Kite-Eating Tree"
 #define R_Version(v,p,s) (((v) * 65536) + ((p) * 256) + (s))
 #define R_MAJOR  "3"
-#define R_MINOR  "4.2"
+#define R_MINOR  "4.3"
 #define R_STATUS ""
 #define R_YEAR   "2017"
-#define R_MONTH  "09"
-#define R_DAY    "28"
-#define R_SVN_REVISION 73368
-#define R_FILEVERSION    3,42,73368,0
+#define R_MONTH  "11"
+#define R_DAY    "30"
+#define R_SVN_REVISION 73796
+#define R_FILEVERSION    3,43,73796,0
 
 #ifdef __cplusplus
 }
