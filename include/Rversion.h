aef6f481ebd754aa5003ab8ca91398d93c7c082d
RJ <ruijie@git>
2021-08-26 13:18:04 +0800

    3.5.1

--- include/Rversion.h
+++ include/Rversion.h
@@ -6,17 +6,17 @@
 extern "C" {
 #endif
 
-#define R_VERSION 197888
-#define R_NICK "Joy in Playing"
+#define R_VERSION 197889
+#define R_NICK "Feather Spray"
 #define R_Version(v,p,s) (((v) * 65536) + ((p) * 256) + (s))
 #define R_MAJOR  "3"
-#define R_MINOR  "5.0"
+#define R_MINOR  "5.1"
 #define R_STATUS ""
 #define R_YEAR   "2018"
-#define R_MONTH  "04"
-#define R_DAY    "23"
-#define R_SVN_REVISION 74626
-#define R_FILEVERSION    3,50,74626,0
+#define R_MONTH  "07"
+#define R_DAY    "02"
+#define R_SVN_REVISION 74947
+#define R_FILEVERSION    3,51,74947,0
 
 #ifdef __cplusplus
 }
