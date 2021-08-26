13934ca40913802053eaa989acd9b4b3025f1af6
RJ <ruijie@git>
2021-08-26 14:13:41 +0800

    4.0.4

--- include/Rversion.h
+++ include/Rversion.h
@@ -6,17 +6,17 @@
 extern "C" {
 #endif
 
-#define R_VERSION 262147
-#define R_NICK "Bunny-Wunnies Freak Out"
+#define R_VERSION 262148
+#define R_NICK "Lost Library Book"
 #define R_Version(v,p,s) (((v) * 65536) + ((p) * 256) + (s))
 #define R_MAJOR  "4"
-#define R_MINOR  "0.3"
+#define R_MINOR  "0.4"
 #define R_STATUS ""
-#define R_YEAR   "2020"
-#define R_MONTH  "10"
-#define R_DAY    "10"
-#define R_SVN_REVISION 79318
-#define R_FILEVERSION    4,03,79318,0
+#define R_YEAR   "2021"
+#define R_MONTH  "02"
+#define R_DAY    "15"
+#define R_SVN_REVISION 80002
+#define R_FILEVERSION    4,04,80002,0
 
 #ifdef __cplusplus
 }
